fc13b3e35ba46a631500f7a76824d33164a79a62
havenfricke <havenfricke@u.boisestate.edu>
2026-01-20 11:08:57 -0700

    player input fix

--- PlayerInput.cs
+++ PlayerInput.cs
@@ -1,26 +1,26 @@
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 
 [RequireComponent(typeof(PlayerCamera))]
 public sealed class PlayerInput : MonoBehaviour
 {
     private InputSystem_Actions actions;
-    private PlayerCamera camera;
+    private PlayerCamera playerCamera;
+    private Vector2 lookInput;
 
     private void Awake()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        playerCamera = GetComponent<PlayerCamera>();
 
-        if (actions == null)
-            actions = new InputSystem_Actions();
-
-        if (camera == null)
-            camera = GetComponent<PlayerCamera>();
+        actions = new InputSystem_Actions();
     }
 
     private void OnEnable()
     {
+        if (actions == null) return;
+        Cursor.lockState = CursorLockMode.Locked;
+
         actions.Player.Look.performed += OnLook;
         actions.Player.Look.canceled += OnLook;
 
@@ -28,28 +28,19 @@ public sealed class PlayerInput : MonoBehaviour
         actions.Player.Move.canceled += OnMove;
 
         actions.Player.Attack.performed += OnAttack;
-        actions.Player.Attack.canceled += OnAttack;
-
         actions.Player.Interact.performed += OnInteract;
-        actions.Player.Interact.canceled += OnInteract;
-
         actions.Player.Crouch.performed += OnCrouch;
-        actions.Player.Crouch.canceled += OnCrouch;
-
         actions.Player.Jump.performed += OnJump;
-        actions.Player.Jump.canceled += OnJump;
-
         actions.Player.Previous.performed += OnPrev;
-        actions.Player.Previous.canceled += OnPrev;
-
         actions.Player.Next.performed += OnNext;
-        actions.Player.Next.canceled += OnNext;
 
         actions.Player.Enable();
     }
 
     private void OnDisable()
     {
+        if (actions == null) return;
+
         actions.Player.Look.performed -= OnLook;
         actions.Player.Look.canceled -= OnLook;
 
@@ -57,36 +48,31 @@ public sealed class PlayerInput : MonoBehaviour
         actions.Player.Move.canceled -= OnMove;
 
         actions.Player.Attack.performed -= OnAttack;
-        actions.Player.Attack.canceled -= OnAttack;
-
         actions.Player.Interact.performed -= OnInteract;
-        actions.Player.Interact.canceled -= OnInteract;
-
         actions.Player.Crouch.performed -= OnCrouch;
-        actions.Player.Crouch.canceled -= OnCrouch;
-
         actions.Player.Jump.performed -= OnJump;
-        actions.Player.Jump.canceled -= OnJump;
-
         actions.Player.Previous.performed -= OnPrev;
-        actions.Player.Previous.canceled -= OnPrev;
-
         actions.Player.Next.performed -= OnNext;
-        actions.Player.Next.canceled -= OnNext;
 
         actions.Player.Disable();
     }
 
+    private void Update()
+    {
+        // Drive these every frame.
+        playerCamera.Look(lookInput);
+    }
+
     private void OnLook(InputAction.CallbackContext context)
     {
-        camera.Look(context.ReadValue<Vector2>());
+        lookInput = context.ReadValue<Vector2>();
     }
 
     private void OnMove(InputAction.CallbackContext context)
     {
-       Debug.Log($"{context.ReadValue<Vector2>()}");
+        Debug.Log("Move action performed");
     }
-        
+
     private void OnAttack(InputAction.CallbackContext context)
     {
         Debug.Log("Attack action performed");
